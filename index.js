11dac641fc66e9043bdefb986c9355090342845e
Trevour <trevour.ambia@student.moringaschool.com>
2025-04-02 01:00:40 +0300

    work done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email
+    };
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(object => {
+        document.body.innerHTML += `<p>ID: ${object.id}</p>`;
+    })
+    .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
+
